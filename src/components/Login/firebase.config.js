4275ed6946da58b18b12bc7e870b8091b9e7e0f2
Asif Mohammad <asifratul45@gmail.com>
2021-02-07 15:33:49 +0600

    commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDeM8jspRm4M2hK0GmtP-oi05iV0jcdOnQ",
+    authDomain: "react-burj-al-arab-website.firebaseapp.com",
+    projectId: "react-burj-al-arab-website",
+    storageBucket: "react-burj-al-arab-website.appspot.com",
+    messagingSenderId: "1066307112632",
+    appId: "1:1066307112632:web:fec2f840ceb09b56beae4d"
+  };
+
+export default firebaseConfig;
